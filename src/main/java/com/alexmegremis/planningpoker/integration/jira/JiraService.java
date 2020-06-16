a2c7a922e2ee4f979a79a1959dcd0fa907b2efbc
Alex Megremis <>
2020-06-16 04:31:19 +0100

    Jira issue retrieval works

--- src/main/java/com/alexmegremis/planningpoker/integration/jira/JiraService.java
+++ src/main/java/com/alexmegremis/planningpoker/integration/jira/JiraService.java
@@ -1,11 +1,14 @@
 package com.alexmegremis.planningpoker.integration.jira;
 
 import com.vaadin.spring.annotation.SpringComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import static org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
@@ -13,19 +16,35 @@ import static org.springframework.web.reactive.function.client.WebClient.Respons
 @SpringComponent
 public class JiraService {
 
+    @Value("${jira.scheme}")
+    private String jiraScheme;
+    @Value("${jira.hostname}")
+    private String jiraHostname;
+    @Value("${jira.port}")
+    private String jiraPort;
+    @Value("${jira.issueAPI}")
+    private String issueAPI;
+    @Value("${jira.user}")
+    private String user;
+    @Value("${jira.pass}")
+    private String pass;
+
     public JiraIssueDTO getIssueByKey(final String issueKey) {
 
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.newInstance()
-                                                                        .scheme("http")
-                                                                        .host("192.168.0.34")
-                                                                        .port("8060")
-                                                                        .pathSegment("rest", "api", "2", "issue", issueKey)
+                                                                        .scheme(jiraScheme)
+                                                                        .host(jiraHostname)
+                                                                        .port(jiraPort)
+//                                                                        .pathSegment(issueAPI, issueKey)
                                                                         .queryParam("fields", "description,summary");
+
+        Arrays.stream(issueAPI.split("/")).forEach(uriComponentsBuilder::pathSegment);
+        uriComponentsBuilder.pathSegment(issueKey);
 
         WebClient webClient = WebClient.builder().build();
         ResponseSpec response = webClient.get()
                                          .uri(uriComponentsBuilder.toUriString())
-                                         .headers(header -> header.setBasicAuth("FIXME", "FIXME"))
+                                         .headers(header -> header.setBasicAuth(user, pass))
                                          .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                                          .retrieve();
 
